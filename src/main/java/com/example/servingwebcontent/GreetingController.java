de6271cc684d23daf5618ef9f9d264f04181f403
rebeccasimmons <60267393+rebeccasimmons@users.noreply.github.com>
2020-05-04 12:08:08 -0400

    Update GreetingController.java

--- src/main/java/com/example/servingwebcontent/GreetingController.java
+++ src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="webpage") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
 	@GetMapping("/welcome")
 	public String welcome(@RequestParam(name="user", required=false, defaultValue="people") String user, Model model) {
 		model.addAttribute("user", user);
 		return "welcome";
+	}
+	@GetMapping("/goodbye")
+	public String goodbye(@RequestParam(name="goodbye", required=false, defaultValue="farewell") String goodbye, Model model) {
+		model.addAttribute("goodbye", goodbye);
+		return "goodbye";
 	}
 }
